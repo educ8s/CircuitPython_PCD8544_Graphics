b8f3628350eb1490cd710aeb393795ebf099082c
educ8s <info@educ8s.tv>
2022-06-17 11:48:36 +0300

    added big numbers font

--- pcd8544_graphics.py
+++ pcd8544_graphics.py
@@ -7,32 +7,178 @@ def drawFullScreenBitmap(display,image):
 def clearScreen(display):
     display.fill(0)
     display.show()
+    
+def dot(x,y,display):
+    display.hline(x + 5,y + 26,3,1)
+    display.hline(x + 4,y + 25,5,1)
+    display.hline(x + 5,y + 24,3,1)
+    
+def zero(x,y,display):
+    topSegment(x,y,display)
+    bottomSegment(x,y,display)
+    topRightSegment(x,y,display)
+    bottomRightSegment(x,y,display)
+    topLeftSegment(x,y,display)
+    bottomLeftSegment(x,y,display)
 
-def drawRect(x,y,width,height,display):
-    for pixel in range(width):
-        display.pixel(x+pixel, y, 1)
-        display.pixel(x+pixel, y+height-1, 1)
+def one(x,y,display):
+    topRightSegment(x,y,display)
+    bottomRightSegment(x,y,display)
     
-    for pixel in range(height):
-        display.pixel(x,y+pixel,1)
-        display.pixel(x+width-1,y+pixel,1)
+def two(x,y,display):
+    topSegment(x,y,display)
+    bottomSegment(x,y,display)
+    topRightSegment(x,y,display)
+    bottomLeftSegment(x,y,display)
+    middleSegment(x,y,display)
+    
+def three(x,y,display):
+    topSegment(x,y,display)
+    bottomSegment(x,y,display)
+    topRightSegment(x,y,display)
+    bottomRightSegment(x,y,display)
+    middleSegment(x,y,display)
+    
+def four(x,y,display):
+    topLeftSegment(x,y,display)
+    topRightSegment(x,y,display)
+    bottomRightSegment(x,y,display)
+    middleSegment(x,y,display)
+
+def five(x,y,display):
+    topSegment(x,y,display)
+    topLeftSegment(x,y,display)
+    middleSegment(x,y,display)
+    bottomRightSegment(x,y,display)
+    bottomSegment(x,y,display)
+    
+def six(x,y,display):
+    topSegment(x,y,display)
+    topLeftSegment(x,y,display)
+    middleSegment(x,y,display)
+    bottomRightSegment(x,y,display)
+    bottomSegment(x,y,display)
+    bottomLeftSegment(x,y,display)
+
+def seven(x,y,display):
+    topSegment(x,y,display)
+    bottomRightSegment(x,y,display)
+    topRightSegment(x,y,display)
+
+def eight(x,y,display):
+    topSegment(x,y,display)
+    bottomSegment(x,y,display)
+    topRightSegment(x,y,display)
+    bottomRightSegment(x,y,display)
+    topLeftSegment(x,y,display)
+    bottomLeftSegment(x,y,display)
+    middleSegment(x,y,display)
+    
+def nine(x,y,display):
+    topSegment(x,y,display)
+    bottomSegment(x,y,display)
+    topRightSegment(x,y,display)
+    bottomRightSegment(x,y,display)
+    topLeftSegment(x,y,display)
+    middleSegment(x,y,display)
+    
+def clear(x,y,display):
+    for number in range(y+27):
+        display.hline(x,number,13,0)
         
-    display.show()
+def middleSegment(x,y,display):
+    x = x + 1
+    y = y + 13
+    display.hline(x,y,11,1)
+    display.hline(x+1,y-1,9,1)
+    display.hline(x+1,y+1,9,1)
     
-def drawLine(x0,y0,x1,y1,display):
-    #Calculate "deltas" of the line (difference between two ending points)
-    dx = x1 - x0
-    dy = y1 - y0
+def topSegment(x,y,display):
+    x = x + 1
+    display.hline(x,y,11,1)
+    x = x + 1
+    y = y + 1
+    display.hline(x,y,9,1)
+    x = x + 1
+    y = y + 1
+    display.hline(x,y,7,1)
+
+def topRightSegment(x,y,display):
+    y=y+1
+    x = x + 12
+    display.vline(x,y,12,1)
+    x = x - 1
+    y = y + 1
+    display.vline(x,y,10,1)
+    x = x - 1
+    y = y + 1
+    display.vline(x,y,8,1)
+
+def bottomLeftSegment(x,y,display):
+    y = y + 14
+    display.vline(x,y,12,1)
+    x = x + 1
+    y = y + 1
+    display.vline(x,y,10,1)
+    x = x + 1
+    y = y + 1
+    display.vline(x,y,8,1)
     
-    #Calculate the line equation based on deltas
-    D = (2 * dy) - dx
-    y = y0
+def topLeftSegment(x,y,display):
+    y=y+1
+    display.vline(x,y,12,1)
+    x = x + 1
+    y = y + 1
+    display.vline(x,y,10,1)
+    x = x + 1
+    y = y + 1
+    display.vline(x,y,8,1)
     
-    for x in range(x0,x1):
-        display.pixel(x,y,1)
-        if D > 0:
-            y = y + 1
-            D = D - 2*dx
-        D = D + 2*dy
+def bottomRightSegment(x,y,display):
+    x = x + 12
+    y = y + 14
+    display.vline(x,y,12,1)
+    x = x - 1
+    y = y + 1
+    display.vline(x,y,10,1)
+    x = x - 1
+    y = y + 1
+    display.vline(x,y,8,1)
+
+def bottomSegment(x,y,display):
+    x = x + 1
+    y = y + 26
+    display.hline(x,y,11,1)
+    x = x + 1
+    y = y - 1
+    display.hline(x,y,9,1)
+    x = x + 1
+    y = y - 1
+    display.hline(x,y,7,1)
     
+def draw_number(number, x, y, display):
+    zeros = '.1f'
+    number_string = f"{number:,{zeros}}"
+    if len(number_string) > 1:
+        for digit in range(len(number_string)):
+            draw_single_number(number_string[digit],x,y,display)
+            x = x + 16
+    else:
+        draw_single_number(number,x,y,display)
+
+def draw_single_number(number,x,y,display):
+    clear(x,y,display)
+    numbers = {"0" : zero,
+           "1" : one,
+           "2" : two,
+           "3" : three,
+           "4" : four,
+           "5" : five,
+           "6" : six,
+           "7" : seven,
+           "8" : eight,
+           "9" : nine,
+           "." : dot}
+    
+    numbers[number](x,y,display)
     display.show()
